7699bc378836c2ee9262c248b80571950e9654a2
MunciYT <munciplays@gmail.com>
2025-12-28 22:42:37 +0100

    Update visitors.js

--- functions/visitors.js
+++ functions/visitors.js
@@ -1,9 +1,14 @@
 export async function onRequest(context) {
   const { DB } = context.env;
 
+  if (!DB) {
+    return new Response("DB missing", { status: 500 });
+  }
+
   const { results } = await DB.prepare(`
     SELECT * FROM visitors
     ORDER BY visited_at DESC
+    LIMIT 100
   `).all();
 
   return new Response(JSON.stringify(results), {
